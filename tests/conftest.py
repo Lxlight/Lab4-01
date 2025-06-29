3d56f2f97cb8480dcfa0cbfa51f4d3e9d5175b33
Lxlight <lulee0830@qq.com>
2025-06-30 02:08:00 +0800

    提交API文档

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
 import pytest
 
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from pygraph import Graph
+
 
 
 @pytest.fixture(scope="function")
